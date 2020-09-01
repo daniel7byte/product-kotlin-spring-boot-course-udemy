8b44f1b577dac85fa958e2249b10cd39e230458d
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2020-09-01 11:41:40 -0500

    First Commit

---
+++ src/main/kotlin/me/daniel7byte/product/controllers/HelloController.kt
@@ -0,0 +1,14 @@
+package me.daniel7byte.product.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class HelloController {
+
+    @GetMapping
+    fun Hello() = "Hello World"
+
+}
