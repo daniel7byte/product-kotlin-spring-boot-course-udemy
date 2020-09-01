e8026cd16ef97a18f34cb7e8a78a5fbd67cf3c56
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2020-09-01 18:03:57 -0500

    Uso del lateinit
    La forma en la que se hace de verdad <3

--- src/main/kotlin/me/daniel7byte/product/controllers/ProductController.kt
+++ src/main/kotlin/me/daniel7byte/product/controllers/ProductController.kt
@@ -1,13 +1,17 @@
 package me.daniel7byte.product.controllers
 
 import me.daniel7byte.product.services.ProductService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/products")
-class ProductController (private val productService: ProductService) {
+class ProductController {
+
+    @Autowired
+    private lateinit var productService: ProductService
 
     @GetMapping
     fun findAll() = productService.findAll()
